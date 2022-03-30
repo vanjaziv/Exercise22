53c5ffd8e87eda53f8f7daefd21ebf315796f79e
vanjaziv <vanja.zivanic1@gmail.com>
2022-03-30 09:31:18 +0200

    lala

--- src/com/company/Mitarbeiter1.java
+++ src/com/company/Mitarbeiter1.java
@@ -5,7 +5,7 @@ public class Mitarbeiter1 {
     String nachname;
     double gehalt;
     int alter;
-    double taxableincome = monatsAbrechnung(28000);
+    double taxableincome = monatsAbrechnung(51000);
     double tax = incomeTax(taxableincome);
 
     public double monatsAbrechnung(double gehalt)
@@ -14,13 +14,22 @@ public class Mitarbeiter1 {
         return taxableincome;
     }
     public double incomeTax(double taxableincome)
-    {
-        if(taxableincome<=10000)
-            tax=(0.1 * 10000);
-        else if(taxableincome>=10000 && taxableincome<=20000)
-            tax=(
-        //else if (taxableincome>=20000 && taxableincome<=30000)
-
+    {    double taxincome = 0;
+        if(taxableincome<=10000){
+            tax=(0.1 * 10000);}
+        else if(taxableincome>=10000 && taxableincome<=20000){
+            taxincome = (taxableincome - 10000);
+            tax=(1000 + (taxincome * 0.2));}
+        else if (taxableincome>=20000 && taxableincome<=30000){
+            taxincome = (taxableincome - 20000);
+            tax=(3000 + (taxincome * 0.32));}
+        else if (taxableincome>=30000 && taxableincome<=50000){
+            taxincome = (taxableincome - 30000);
+            tax=(6200 + (taxincome * 0.45));}
+        else if (taxableincome>50000){
+            taxincome = (taxableincome - 50000);
+            tax=(15200 + (taxincome * 0.60));
+            }
         return tax;
     }
 }
