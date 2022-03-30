0d4cc90425abd1fbcb78ebb58bc2ec0759a213ca
vanjaziv <99715839+vanjaziv@users.noreply.github.com>
2022-03-30 08:04:17 +0200

    Update Mitarbeiter1.java

--- src/com/company/Mitarbeiter1.java
+++ src/com/company/Mitarbeiter1.java
@@ -14,11 +14,12 @@ public class Mitarbeiter1 {
         return taxableincome;
     }
     public double incomeTax(double taxableincome)
-    {
+    {.  double taxincome = 0;
         if(taxableincome<=10000)
             tax=(0.1 * 10000);
         else if(taxableincome>=10000 && taxableincome<=20000)
-            tax=(
+            taxincome = (taxableincome - 10000)
+            tax=(1000 + taxincom * 0.2)
         //else if (taxableincome>=20000 && taxableincome<=30000)
 
         return tax;
